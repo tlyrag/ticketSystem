ad442280589a67020fc21e94dcbb4b16d765b490
=Thiago Lyra <tlyraganem@gmail.com>
2024-05-31 10:16:50 -0700

    Adding Filter Search by term

---
+++ Client/Test.js
@@ -0,0 +1,7 @@
+const a = ['6657b576e7af648d60f2d277', 'TEsting', 'Medium', 'Bug', 'Issue in something', '2024-05-29', Array(0), 0, '2024-05-31T15:00:55.481Z', 'UserName', '-', '-', 'Working'];
+const b = a.find(el => el.includes('Med'))
+if(b) {
+    console.log('True')
+}
+
+//console.log(b)
