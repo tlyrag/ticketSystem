477968d4a964e991c7ee3d42d036fed623669810
Thiago Lyra <tlyra@hq.westkeyprint.com>
2024-06-11 12:51:08 -0700

    adding plotly

---
+++ Client/src/Views/Dashboard/InventoryCharts/InventoryPlotlyChart.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+import { DefaultTooltipContent } from 'recharts';
+
+
+const InventPlt = () => {
+    return (
+        <Plot
+        data={[
+
+          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        ]}
+        layout={{width: 720, height: 640, title: 'A Fancy Plot'}}
+      />
+    )
+}
+
+export default InventPlt;
--- Client/src/Views/Dashboard/InventoryDash.jsx
+++ Client/src/Views/Dashboard/InventoryDash.jsx
@@ -1,11 +1,12 @@
 import InventoryBarChart from "./InventoryCharts/InventoryBarChart";
+import InventoryPlotlyChart from "./InventoryCharts/InventoryPlotlyChart"
 
 const Inventory =() => {
     return (
     <div className="h-full bg-white drop-shadow-3xl grid grid-rows-3 grid-flow-col gap-4 m-15">
            <div className="grid grid-cols-2 gap-4 ">
                 <div><InventoryBarChart/></div> 
-                <div><InventoryBarChart/></div> 
+                <div><InventoryPlotlyChart/></div> 
             </div> 
            
     </div>
