c8411e088f1722b38e1782891e86d86f5eb89213
Thiago Lyra <tlyra@hq.westkeyprint.com>
2024-06-26 15:42:17 -0700

    Adding integrationwith qm

---
+++ Server/controller/QuantumServer.Controller.js
@@ -0,0 +1,32 @@
+import quantumModel from '../model/QuantumServerModel.js'
+const runInventory =(company) => {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body:JSON.stringify( {
+            query_name: "inventory",
+            params: [company]
+        })
+    };
+    return fetch(quantumModel.runQuery(),requestOptions).then(response=>response.json())
+    .then(data=> {
+        return data
+    })
+    .catch(err=> console.log(`Failed to run query ${err}`))
+    
+}
+
+const test = () => {
+    return fetch(quantumModel.testServer())
+    .then(response=>response.json())
+    .then(data=> {
+        return data
+    }).catch(err => {
+        console.log(`Failed to fetch  Data ${err}`)
+    })
+}
+
+export default {
+    runInventory,
+    test
+}
